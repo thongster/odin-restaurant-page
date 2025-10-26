2e5eaba9c38773b18ae9d8c685f90415a6f51d68
thongster <106802301+thongster@users.noreply.github.com>
2025-10-26 20:11:02 +0700

    added footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,1 +1,16 @@
+function createFooter() {
+    const body = document.querySelector("body");
+    const footer = document.createElement("footer");
+    const sourceCode = document.createElement("a");
+    sourceCode.textContent = "Source Code";
+
+    sourceCode.href = "https://github.com/thongster/odin-restaurant-page";
+    sourceCode.target = "_blank";
+    sourceCode.rel = "noreferrer";
+
+    body.append(footer);
+    footer.append("Developed by Thongster | ")
+    footer.append(sourceCode)
+};
+
 export {createFooter};
