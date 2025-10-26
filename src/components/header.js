fdbd7971d818e037fbbf8e3423ddbf7b67b9cf71
thongster <106802301+thongster@users.noreply.github.com>
2025-10-26 19:46:49 +0700

    basic layout, header, nav, no functionality yet

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import logo from "../assets/images/icon.png";
+
+function createHeader() {
+    const headerSection = document.querySelector("header");
+    const headerMain = document.createElement("div");
+    headerMain.classList.add("headerMain");
+    const headerLogo = document.createElement("img");
+    const headerText = document.createElement("h1");
+
+    headerLogo.src = logo;
+    headerLogo.alt = "sông & steam logo";
+    
+    headerSection.append(headerMain);
+    headerMain.append(headerLogo);
+    headerMain.append(headerText);
+    headerText.textContent = "sông & steam";
+};
+
+export {createHeader};
---
+++ src/components/nav.js
@@ -0,0 +1,22 @@
+function createNav() {
+    const content = document.querySelector("#content");
+    const nav = document.createElement("div");
+    nav.classList.add("nav");
+    const home = document.createElement("button");
+    const menu = document.createElement("button");
+    const about = document.createElement("button");
+    const contact = document.createElement("button");
+
+    home.textContent = "home";
+    menu.textContent = "menu";
+    about.textContent = "about";
+    contact.textContent = "contact";
+
+    content.append(nav);
+    nav.append(home);
+    nav.append(menu);
+    nav.append(about);
+    nav.append(contact);
+};
+
+export {createNav};
